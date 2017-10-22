44bdbf4a207794d86b51f061d77c159c97ddec50
grapswiz <ineeza@gmail.com>
2017-10-22 18:11:39 +0900

    funiki(go): ふんいき

--- go/src/isubata/app.go
+++ go/src/isubata/app.go
@@ -431,13 +431,18 @@ func getMessage(c echo.Context) error {
 	}
 
 	if len(messages) > 0 {
-		_, err := db.Exec("INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at)"+
-			" VALUES (?, ?, ?, NOW(), NOW())"+
-			" ON DUPLICATE KEY UPDATE message_id = ?, updated_at = NOW()",
-			userID, chanID, messages[0].ID, messages[0].ID)
-		if err != nil {
-			return err
-		}
+		errChan := make(chan error, 0)
+		go func() {
+			_, err := db.Exec("INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at)"+
+				" VALUES (?, ?, ?, NOW(), NOW())"+
+				" ON DUPLICATE KEY UPDATE message_id = ?, updated_at = NOW()",
+				userID, chanID, messages[0].ID, messages[0].ID)
+			if err != nil {
+				errChan <- err
+			}
+			err = <-errChan
+		}()
+
 	}
 
 	return c.JSON(http.StatusOK, response)
